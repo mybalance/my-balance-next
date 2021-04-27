866bc388a0786008b379f7dcde9d04b1e575ceeb
Sam Drew <sam-drew@outlook.com>
2021-04-27 08:02:38 +0100

    :sparkles: + icon to nav brand

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,16 @@ const home = () => {
     {/* Navigation */}
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">MyBalance x Abri</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+          <img
+            src="/assets/img/Web_Icon_1024px.png"
+            width="50"
+            height="50"
+            className="d-inline-block align-top pr-1"
+            alt="MyBalance supported by Abri logo"
+          />
+          MyBalance
+        </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
           data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
           aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
