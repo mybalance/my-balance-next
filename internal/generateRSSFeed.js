d3ce7c774d2940935bbc5d49069b1e916429a04b
Sam Drew <sam-drew@outlook.com>
2022-02-21 14:22:22 +0000

    :sparkles: added rss feed for blog

---
+++ internal/generateRSSFeed.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import { Feed } from "feed";
+
+export const generateRSSFeed = (articles) => {
+    const baseUrl = 'https://mybalance.earth';
+    const author = {
+        name: 'MyBalance',
+        email: 'media@mybalance.earth',
+        link: 'https://mybalance.earth/blog',
+    };
+
+    // Construct a new Feed object
+    const feed = new Feed({
+        title: 'MyBalance Newsfeed',
+        description:
+            "",
+        id: baseUrl,
+        link: baseUrl,
+        language: 'en',
+        feedLinks: {
+            rss2: `${baseUrl}/blog/rss.xml`,
+        },
+        author,
+    });
+
+    // Add each article to the feed
+    articles.forEach((post) => {
+        const url = `${baseUrl}/blog/${post.slug}`;
+
+        feed.addItem({
+            title: post.frontmatter.title,
+            id: url,
+            link: url,
+            description: post.frontmatter.description,
+            content: post.content,
+            author: [author],
+            date: new Date(post.frontmatter.date),
+        });
+    });
+
+    // Write the RSS output to a public file
+    fs.writeFileSync('public/blog/rss.xml', feed.rss2());
+    console.log("Written out RSS feed")
+};
